4987f3708af1d2debeff7427bddc976771b4bc47
David Farquharson <djfarquharson@spscommerce.com>
2014-08-28 13:05:09 -0500

    cap tiles to 30. resize tiles automagically. have to actually call functions for them to work (dumbass).

--- js/keylistener.js
+++ js/keylistener.js
@@ -29,30 +29,38 @@ function keysPressed(e) {
     keys[e.keyCode] = true;
     updateKeyChord(e);
 
-    if (prefix && keys[49]) {        // Ctrl Shift 1
-        fn_and_prevent(e, focus_tile, [0]);
-    } else if (prefix && keys[50]) { // Ctrl Shift 2
-        fn_and_prevent(e, focus_tile, [1]);
-    } else if (prefix && keys[51]) { // Ctrl Shift 3
-        fn_and_prevent(e, focus_tile, [2]);
-    } else if (prefix && keys[52]) { // Ctrl Shift 4
-        fn_and_prevent(e, focus_tile, [3]);
-    } else if (prefix && keys[53]) { // Ctrl Shift 5
-        fn_and_prevent(e, focus_tile, [4]);
-    } else if (prefix && keys[54]) { // Ctrl Shift 6
-        fn_and_prevent(e, focus_tile, [5]);
-    } else if (prefix && keys[55]) { // Ctrl Shift 7
-        fn_and_prevent(e, focus_tile, [6]);
-    } else if (prefix && keys[56]) { // Ctrl Shift 8
-        fn_and_prevent(e, focus_tile, [7]);
-    } else if (prefix && keys[57]) { // Ctrl Shift 9
-        fn_and_prevent(e, focus_tile, [8]);
-    } else if (prefix && keys[84]) { // Ctrl Shift T
+    if (prefix() && keys[49]) {        // Ctrl Shift 1
+        fn_and_prevent(e, focusTile, [0]);
+    } else if (prefix() && keys[50]) { // Ctrl Shift 2
+        fn_and_prevent(e, focusTile, [1]);
+    } else if (prefix() && keys[51]) { // Ctrl Shift 3
+        fn_and_prevent(e, focusTile, [2]);
+    } else if (prefix() && keys[52]) { // Ctrl Shift 4
+        fn_and_prevent(e, focusTile, [3]);
+    } else if (prefix() && keys[53]) { // Ctrl Shift 5
+        fn_and_prevent(e, focusTile, [4]);
+    } else if (prefix() && keys[54]) { // Ctrl Shift 6
+        fn_and_prevent(e, focusTile, [5]);
+    } else if (prefix() && keys[55]) { // Ctrl Shift 7
+        fn_and_prevent(e, focusTile, [6]);
+    } else if (prefix() && keys[56]) { // Ctrl Shift 8
+        fn_and_prevent(e, focusTile, [7]);
+    } else if (prefix() && keys[57]) { // Ctrl Shift 9
+        fn_and_prevent(e, focusTile, [8]);
+    } else if (prefix() && keys[84]) { // Ctrl Shift T
         fn_and_prevent(e, makeNewTile);
-    } else if (prefix && keys[67]) { // Ctrl Shift C
+    } else if (prefix() && keys[67]) { // Ctrl Shift C
         fn_and_prevent(e, cloneSelectedTile);
-    } else if (prefix && keys[68]) { // Ctrl Shift D
+    } else if (prefix() && keys[68]) { // Ctrl Shift D
         fn_and_prevent(e, deleteSelectedTile);
+    } else if (prefix() && (keys[72] || keys[37])) { // Ctrl Shift H/Left
+        fn_and_prevent(e, increase_tile_size_left);
+    } else if (prefix() && (keys[74] || keys[40])) { // Ctrl Shift J/Down
+        fn_and_prevent(e, increase_tile_size_down);
+    } else if (prefix() && (keys[75] || keys[38])) { // Ctrl Shift K/Up
+        fn_and_prevent(e, increase_tile_size_up);
+    } else if (prefix() && (keys[76] || keys[39])) { // Ctrl Shift L/Right
+        fn_and_prevent(e, increase_tile_size_right);
     } else if (keys[17] && keys[186]) {
         fn_and_prevent(e, log, ['Focus dat minibuffer']);
     } else if (keys[17] && keys[70]) {
--- js/tilemanager.js
+++ js/tilemanager.js
@@ -11,22 +11,53 @@ window.addEventListener('resize', displayViewportSize, false);
 var tiles = ['tile 1', 'tile 2', 'tile 3', 'tile 4', 'tile 5',
              'tile 6', 'tile 7', 'tile 8', 'tile 9']
 
-var focus_tile = function (n) {
+var focusTile = function (n) {
     console.log('focusing on ' + tiles[n]);
+}
+
+function increase_tile_size_left() {
+    console.log("increase tile size left.");
+}
+
+function increase_tile_size_down() {
+    console.log("increase tile size down.");
+}
+
+function increase_tile_size_up() {
+    console.log("increase tile size up.");
+}
+
+function increase_tile_size_right() {
+    console.log("increase tile size right.");
+}
+
+var resizeAllTiles = function () {
+    //do the maths and do the resize based on # of children
+    var len = __tileHolder.children.length;
+    for (var i = 0; i < len; i++) {
+        var currentTile = __tileHolder.children[i];
+        console.log(currentTile);
+        if (currentTile.getAttribute('id') === 'tile') {
+            currentTile.style.height = __tileHolder.offsetHeight - 10 + 'px';
+            currentTile.style.width = ((1/len) * 100) - 0.4 + '%';
+        }
+    }
 }
 
 var __tileHolder = document.getElementById('tile-holder');
 
 function makeNewTile() {
-    var newTile = document.createElement('div');
-    var label = document.createElement('label');
-    newTile.setAttribute('id', 'tile');
-    __tileHolder.appendChild(newTile);
-    newTile.appendChild(label);
-    label.textContent = __tileHolder.children.length;
-    console.log('make new tile');
-    console.log(__tileHolder.children.length);
-    console.log(newTile);
+    if (__tileHolder.children.length < 30) {
+        var newTile = document.createElement('div');
+        var content = document.createElement('label');
+        newTile.setAttribute('id', 'tile');
+        __tileHolder.appendChild(newTile);
+        newTile.appendChild(content);
+        content.textContent = __tileHolder.children.length;
+        resizeAllTiles();
+    } else {
+        console.log("do you really need more than 30 tiles?")
+    }
 }
 
 function cloneSelectedTile() {
@@ -36,5 +67,6 @@ function cloneSelectedTile() {
 function deleteSelectedTile() {
     if (__tileHolder.lastChild !== null) {
         __tileHolder.removeChild(__tileHolder.lastChild);
+        resizeAllTiles();
     }
 }
